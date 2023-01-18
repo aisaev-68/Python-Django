5314aec631e4ecc91926d3f3eed8fb06865c48f4
ramazan_aisaev <aisaev-68@yandex.ru>
2023-01-18 11:35:56 +0300

    1

--- M_02_DjangoIntro/mysite/shopapp/management/commands/create_products.py
+++ M_02_DjangoIntro/mysite/shopapp/management/commands/create_products.py
@@ -9,16 +9,7 @@ from shopapp.models import Product
 class Command(BaseCommand):
     help = 'Import data from csv file'
 
-    # def add_arguments(self, parser):
-    #     parser.add_argument('name', type=str)
-    #     parser.add_argument('description', type=str)
-    #     parser.add_argument('price', type=float)
-
     def handle(self, *args, **kwargs):
-        # name = options['name']
-        # description = options['description']
-        # price = options['price']
-
         BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         file_path = f"{BASE_DIR}/commands/products.csv"
         print(file_path)
@@ -31,5 +22,5 @@ class Command(BaseCommand):
                     Product.objects.get_or_create(**row)
 
                 except AttributeError as error:
-                    print('Модели нет')
+                    print(error)
                     break
