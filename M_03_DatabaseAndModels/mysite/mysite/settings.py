f26f3aab7c0daeee6037f9d25445c1ab16eb95ae
ramazan_aisaev <aisaev-68@yandex.ru>
2023-01-24 07:49:42 +0300

    fix

--- M_03_DatabaseAndModels/mysite/mysite/settings.py
+++ M_03_DatabaseAndModels/mysite/mysite/settings.py
@@ -28,7 +28,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'homework.urls'
+ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
     {
@@ -46,7 +46,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'homework.wsgi.application'
+WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
 # Database
