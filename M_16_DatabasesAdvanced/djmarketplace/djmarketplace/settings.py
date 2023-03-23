6fb2d3ee15d16a3f72171162992b3682fba68df4
ramazan_aisaev <aisaev-68@yandex.ru>
2023-03-23 15:15:16 +0300

    fix33

--- M_16_DatabasesAdvanced/djmarketplace/djmarketplace/settings.py
+++ M_16_DatabasesAdvanced/djmarketplace/djmarketplace/settings.py
@@ -92,9 +92,14 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
+    'PAGE_SIZE': 20,
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        ]
 }
 
 
