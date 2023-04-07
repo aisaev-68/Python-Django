5e5d51f50512fb7ddb121d322f03c1da18fbd2dc
ramazan_aisaev <aisaev-68@yandex.ru>
2023-04-07 14:58:39 +0300

    fix

--- M_16_DatabasesAdvanced/djmarketplace/csv_to_json.py
+++ M_16_DatabasesAdvanced/djmarketplace/csv_to_json.py
@@ -19,37 +19,37 @@ with open('phones.csv', 'r', newline='') as csvfile:
         s = BeautifulSoup(row["Характеристики (HTML/Table)"], 'html.parser')
         d = s.findAll('td')
         # lst.append(s)
-        print(d)
+
         l = []
         data = {}
         for ind, item in enumerate(d):
 
             if (ind + 1) % 2 == 0:
-                data[str(BeautifulSoup(str(d[ind - 1]), 'html.parser').get_text())] = str(BeautifulSoup(str(d[ind]), 'html.parser').get_text())
+                data[str(BeautifulSoup(str(d[ind - 1]), 'html.parser').get_text().replace("\\", ""))] = str(BeautifulSoup(str(d[ind]), 'html.parser').get_text()).replace(" ", "").replace("\\", "")
 
-        print(data)
 
-        break
-    #     if len(a) > 1:
-    #         link = a[1:]
-    #     else:
-    #         link = a
-    #
-    #     product = {
-    #         "name": row["Имя товара"][18:],
-    #         "brand": row["Производитель"],
-    #         "description": row["Описание"],
-    #         "attributes": row["Характеристики (HTML/Table)"],
-    #         "rating": round(random.uniform(0, 5), 1),
-    #         "image": link,
-    #         "price": row["Цена"].replace(" ", "").replace(",", "."),
-    #         "discount": random.choices([3, 5, 10, 15, 20])[0],
-    #         "products_count": 50,
-    #         "archived": False,
-    #         "sold": random.choices([x + 1 for x in range(50)])[0],
-    #         }
-    #     lst.append(product)
-    # print(len(lst))
-    # prod["phones"] = lst
-    # with open('json_data-phones.json', mode='w', encoding='utf-8') as f:
-    #     json.dump(prod, f, ensure_ascii=False, indent=4)
+
+
+        if len(a) > 1:
+            link = a[1:]
+        else:
+            link = a
+
+        product = {
+            "name": row["Имя товара"][18:],
+            "brand": row["Производитель"],
+            "description": row["Описание"],
+            "attributes": data,
+            "rating": round(random.uniform(0, 5), 1),
+            "image": link,
+            "price": row["Цена"].replace(" ", "").replace(",", "."),
+            "discount": random.choices([3, 5, 10, 15, 20])[0],
+            "products_count": 50,
+            "archived": False,
+            "sold": random.choices([x + 1 for x in range(50)])[0],
+            }
+        lst.append(product)
+
+    prod["phones"] = lst
+    with open('json_data-phones.json', mode='w', encoding='utf-8') as f:
+        json.dump(prod, f, ensure_ascii=False, indent=4)
--- M_16_DatabasesAdvanced/djmarketplace/shopapp/management/commands/create_products.py
+++ M_16_DatabasesAdvanced/djmarketplace/shopapp/management/commands/create_products.py
@@ -28,55 +28,70 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         self.stdout.write("Create products")
-        with open(str(Path(__file__).parent.joinpath('json_data-products.json'))) as json_file:
-            data = json.load(json_file)["data"]
+        with open(str(Path(__file__).parent.joinpath('json_data-phones.json'))) as json_file:
+            data = json.load(json_file)["phones"]
         prod_len = len(data)
         user = User.objects.filter(username='editor').first()
         shops = [s.pk for s in Shop.objects.all()]
 
         for value in data:
             start_shop = sorted(random.sample(shops, 3))
-            request = requests.get(value.get('image')[0])
-            filename = str(uuid.uuid4())
-            file_name = "{name}.{ext}".format(name=filename, ext='jpg')
-            file_path = "product_images/{new_dir}/{image_name}".format(new_dir=new_dir_file,
-                                                                       image_name=file_name)
-            path_absolute = str(Path(uploaded_file_path, file_name))
-            with open(path_absolute, 'wb') as f:
-                f.write(request.content)
-
-            discount = value.get('discount')
-            price = decimal.Decimal(value.get('price'))
-            new_price = (price - price * discount / 100)
-
-            product = Product.objects.create(
-                        name=value.get('name'),
-                        brand=value.get('brand'),
-                        description=value.get('description'),
-                        attributes=value.get('attributes'),
-                        rating=value.get('rating'),
-                        created_by=user,
-                        price=price,
-                        new_price=new_price,
-                        discount=discount,
-                        image=file_path,
-                        products_count=50,
-                        sold=value.get('sold'),
-                        archived=value.get('archived'),
-                    )
-            for idx in start_shop:
-                ShopItem.objects.create(shop_id=idx, product_id=product.pk)
-
-            self.stdout.write(self.style.SUCCESS(f"Product {product} created"))
+            error = {}
+            try:
+                request = requests.get(value.get('image')[0])
+                request.raise_for_status()
+            except requests.exceptions.HTTPError as errh:
+                error = {"Error": errh}
+            except requests.exceptions.ConnectionError as errc:
+                error = {"Error": errc}
+            except requests.exceptions.Timeout as errt:
+                error = {"Error": errt}
+            except requests.exceptions.RequestException as err:
+                error = {"Error": err}
 
 
+            if not error.get("Error"):
+
+                filename = str(uuid.uuid4())
+                file_name = "{name}.{ext}".format(name=filename, ext='jpg')
+                file_path = "product_images/{new_dir}/{image_name}".format(new_dir=new_dir_file,
+                                                                           image_name=file_name)
+                path_absolute = str(Path(uploaded_file_path, file_name))
+                with open(path_absolute, 'wb') as f:
+                    f.write(request.content)
+
+                discount = value.get('discount')
+                price = decimal.Decimal(value.get('price'))
+                new_price = (price - price * discount / 100)
+
+                product = Product.objects.create(
+                            name=value.get('name'),
+                            brand=value.get('brand'),
+                            description=value.get('description'),
+                            attributes=value.get('attributes'),
+                            rating=value.get('rating'),
+                            created_by=user,
+                            price=price,
+                            new_price=new_price,
+                            discount=discount,
+                            image=file_path,
+                            products_count=50,
+                            sold=value.get('sold'),
+                            archived=value.get('archived'),
+                        )
+                for idx in start_shop:
+                    ShopItem.objects.create(shop_id=idx, product_id=product.pk)
+
+                self.stdout.write(self.style.SUCCESS(f"Product {product} created"))
 
 
-        # pks_to_delete = []
-        # rows = Product.objects.values_list('pk', 'name').order_by('name')
-        # filter_func = lambda x: x[1]
-        # for key, group in itertools.groupby(rows.iterator(), filter_func):
-        #     pks_to_delete.extend((i[0] for i in list(group)[1:]))
-        #
-        # Product.objects.filter(pk__in=pks_to_delete).delete()
+
+
+        pks_to_delete = []
+        rows = Product.objects.values_list('pk', 'name').order_by('name')
+        filter_func = lambda x: x[1]
+        for key, group in itertools.groupby(rows.iterator(), filter_func):
+            pks_to_delete.extend((i[0] for i in list(group)[1:]))
+
+        Product.objects.filter(pk__in=pks_to_delete).delete()
         self.stdout.write(self.style.SUCCESS("Products created"))
