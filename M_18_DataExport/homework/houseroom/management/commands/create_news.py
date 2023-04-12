1e4b33f0c7ef889f9f8bc570a4b260c6091b5e3a
ramazan_aisaev <aisaev-68@yandex.ru>
2023-04-12 16:48:09 +0300

    fix

---
+++ M_18_DataExport/homework/houseroom/management/commands/create_news.py
@@ -0,0 +1,55 @@
+import os
+import random
+import uuid
+from pathlib import Path
+
+from django.utils.timezone import now
+from mimesis import Text, Internet
+from mimesis.locales import Locale
+import requests
+from django.contrib.auth.models import User
+from django.core.management import BaseCommand
+
+from news.models import News, NewsImage
+
+
+
+new_dir_file = now().date().strftime("%Y/%m/%d")
+uploaded_file_path = Path().parent / "media/news_images" / new_dir_file
+uploaded_file_path.mkdir(exist_ok=True, parents=True)
+uploaded_file_path = uploaded_file_path.absolute()
+
+class Command(BaseCommand):
+    """
+    Creates products
+    """
+
+    def handle(self, *args, **options):
+        self.stdout.write("Create news")
+        users = [user for user in User.objects.all()]
+
+        for _ in range(20):
+            user = random.choice(users)
+            text = Text(locale=Locale.RU)
+            internet = Internet()
+
+            url = internet.stock_image(width=1920, height=1080)
+            request = requests.get(url)
+            filename = str(uuid.uuid4())
+            file_name = "{name}.{ext}".format(name=filename, ext='jpg')
+            file_path = "news_images/{new_dir}/{image_name}".format(new_dir=new_dir_file, image_name=file_name)
+            path_absolute = str(Path(uploaded_file_path, file_name))
+            with open(path_absolute, 'wb') as f:
+                f.write(request.content)
+
+            news = News.objects.create(
+                title=text.title(),
+                description=text.text(),
+            )
+            news_image = NewsImage(
+                news=news,
+                image=file_path,
+            )
+            news_image.save()
+
+        self.stdout.write(self.style.SUCCESS("News created"))
--- M_18_DataExport/homework/news/models.py
+++ M_18_DataExport/homework/news/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.utils.timezone import now
+from PIL import Image
+
+
+def get_upload_path_by_posts(instance, filename):
+    return os.path.join('post_images/', now().date().strftime("%Y/%m/%d"), filename)
 
 
 class News(models.Model):
@@ -15,3 +21,23 @@ class News(models.Model):
 
     def __str__(self):
         return "News {t}".format(t=self.title)
+
+
+class NewsImage(models.Model):
+    news = models.ForeignKey(News, on_delete=models.CASCADE, related_name='news_images', verbose_name=_('News'))
+    image = models.FileField(upload_to=get_upload_path_by_posts, blank=True, verbose_name=_('Files post'))
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)
+        if img.height > 850 or img.width > 450:
+            img.thumbnail((850, 450))
+        img.save(self.image.path, quality=70, optimize=True)
+
+    def __str__(self):
+        return "NewsImage {image}".format(image=self.image)
+
+    class Meta:
+        verbose_name = _("News image")
+        verbose_name_plural = _("News images")
+        ordering = ["news", ]
